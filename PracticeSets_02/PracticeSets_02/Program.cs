05f798124af0decfa89c6dc580dd1f244e3410a7
Pamperloth <125177508+Pamperloth@users.noreply.github.com>
2024-08-21 20:58:09 +0800

    Trying again

--- PracticeSets_02/PracticeSets_02/Program.cs
+++ PracticeSets_02/PracticeSets_02/Program.cs
@@ -10,15 +10,8 @@ namespace PracticeSets_02
     {
         static void Main(string[] args)
         {
-            /*
-            Console.WriteLine("Even Numbers");
-            for (int i = 1; i < 20; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
+
+            "ENTER EVEN CODE HERE"
         }
     }
 }
